a9a19d351959fbfa6a3b138a0d0236758139daae
Anas <anasansari2357@gmail.com>
2025-05-11 16:23:36 +0530

    Implement mocked chat bot

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import HowItWorks from './components/HowItWorks'
 import WhyChooseUs from './components/WhyChooseUs'
 import Testimonials from './components/Testimonials'
 import ContactForm from './components/ContactForm'
+import ChatBox from './components/ChatBot'
 
 function App() {
   return (
     <>
+        <ChatBox />
         <Hero />
         <HowItWorks />
         <WhyChooseUs />
