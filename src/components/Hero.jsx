a3dbc2fa671560b59b6b0f0b2cdc35e0dcc86676
Anas <anasansari2357@gmail.com>
2025-05-11 15:14:00 +0530

    Implement moving blobs in hero section to improve visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,16 @@ function Hero() {
     return (
         <section className="relative h-screen flex items-center justify-center text-white bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e] overflow-hidden">
             <motion.div
+                className="absolute top-10 left-[-50px] w-[300px] h-[300px] bg-pink-500 rounded-full filter blur-3xl opacity-20"
+                animate={{ y: [0, 300, 0], x: [0, 400, 0] }}
+                transition={{ duration: 14, repeat: Infinity, ease: "easeInOut" }}
+            />
+            <motion.div
+                className="absolute bottom-10 right-[-50px] w-[300px] h-[300px] bg-blue-500 rounded-full filter blur-3xl opacity-20"
+                animate={{ y: [0, -200, 0] }}
+                transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
+            />
+            <motion.div
                 className="z-10 text-center px-4 w-2xl"
                 initial={{ opacity: 0, y: 30 }}
                 animate={{ opacity: 1, y: 0 }}
